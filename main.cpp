34adfaebfdb8dea54ca55fc4df2f2041bcd449b4
zed07 <houssine199222@gmail.com>
2024-12-15 17:24:09 -0500

    Create functions to retrieve IP addresses

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <cstring>
 
 int main()
 {
+    std::string url;
+    addrinfo hints;
+    addrinfo *servinfo;
+
+    std::memset(&hints, 0, sizeof hints);
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    std::cout << "Please enter a domain name" << std::endl;
+
+    std::cin >> url;
+
+    int status = getaddrinfo(url.c_str(), NULL, &hints, &servinfo);
+
+    if (status != 0)
+    {
+        std::cout << "Error: " << gai_strerror(status) << std::endl;
+    }
+
+    char ipv4str[INET_ADDRSTRLEN];
+
+    while (servinfo != nullptr)
+    {
+        sockaddr_in *ipv4 = (sockaddr_in *)servinfo->ai_addr;
+
+        inet_ntop(AF_INET, &(ipv4->sin_addr), ipv4str, sizeof(ipv4str));
+
+        std::cout << "The IPv4 Address is: " << ipv4str << std::endl;
+
+        servinfo = servinfo->ai_next;
+    }
+
+    freeaddrinfo(servinfo);
 
     return 0;
 }
