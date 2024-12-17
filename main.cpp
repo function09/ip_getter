46a5bb68d99f6ce5127f2910aaa9505de5545d2a
zed07 <houssine199222@gmail.com>
2024-12-17 00:49:11 -0500

    Correct freeaddrinfo call

--- main.cpp
+++ main.cpp
@@ -8,38 +8,43 @@ int main()
 {
     std::string url;
     addrinfo hints;
-    addrinfo *servinfo;
+    addrinfo *servInfo;
+    addrinfo *originalServInfo;
 
     std::memset(&hints, 0, sizeof hints);
 
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
-    std::cout << "Please enter a domain name" << std::endl;
+    std::cout << "Please enter a domain name: " << std::endl;
 
     std::cin >> url;
 
-    int status = getaddrinfo(url.c_str(), NULL, &hints, &servinfo);
+    int status = getaddrinfo(url.c_str(), NULL, &hints, &servInfo);
 
     if (status != 0)
     {
-        std::cout << "Error: " << gai_strerror(status) << std::endl;
+        std::cerr << "Error: " << gai_strerror(status) << std::endl;
+
+        return 1;
     }
 
     char ipv4str[INET_ADDRSTRLEN];
 
-    while (servinfo != nullptr)
+    originalServInfo = servInfo;
+
+    while (servInfo != nullptr)
     {
-        sockaddr_in *ipv4 = (sockaddr_in *)servinfo->ai_addr;
+        sockaddr_in *ipv4 = (sockaddr_in *)servInfo->ai_addr;
 
         inet_ntop(AF_INET, &(ipv4->sin_addr), ipv4str, sizeof(ipv4str));
 
         std::cout << "The IPv4 Address is: " << ipv4str << std::endl;
 
-        servinfo = servinfo->ai_next;
+        servInfo = servInfo->ai_next;
     }
 
-    freeaddrinfo(servinfo);
+    freeaddrinfo(originalServInfo);
 
     return 0;
 }
